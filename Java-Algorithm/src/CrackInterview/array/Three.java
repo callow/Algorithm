c2dcfe368a4bd96920a3120f779229c82ad8363a
Songlei0704 <Songlei0704@14087-NB.cse.com.sg>
2017-07-26 16:27:31 +0800

    answers

--- Java-Algorithm/src/CrackInterview/array/Three.java
+++ Java-Algorithm/src/CrackInterview/array/Three.java
@@ -1,20 +1,16 @@
 package CrackInterview.array;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Three {
 	// 可以用linkedHashSet去遍历，也可以用java8自动获得
 	public static void main(String[] args) {
-		String input = "ascccd0s";
-		removeDuplicate(input);
+		removeDuplicate("ascccd0s");
 	}
 	
 	public static void removeDuplicate(String input) {
 		
-		String[] ary = input.split(""); // 把String换成StringArray
+		String[] ary = input.split(""); // 把String换成String[]
 		
 		Arrays.asList(ary).stream().distinct().forEach(System.out::print);
 		

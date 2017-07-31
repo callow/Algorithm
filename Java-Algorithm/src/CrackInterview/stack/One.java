883406ddbee1e3b60a4f18b88e3b14e46e04bcec
Songlei0704 <Songlei0704@14087-NB.cse.com.sg>
2017-07-31 17:11:28 +0800

    update

---
+++ Java-Algorithm/src/CrackInterview/stack/One.java
@@ -0,0 +1,12 @@
+package CrackInterview.stack;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+public class One {
+	public static void main(String[] args) {
+		String[] inputs =  String.valueOf(123456789).split("");
+		Collections.reverse(Arrays.asList(inputs));
+		
+	}
+}

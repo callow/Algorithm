584050d5cbc1e93eff0872137b7326bfe0a0f6e1
Songlei0704 <Songlei0704@14087-NB.cse.com.sg>
2017-08-03 15:14:51 +0800

    update

--- Java-Algorithm/src/leetcode/Remove_Duplicates_from_Sorted_Array_26.java
+++ Java-Algorithm/src/leetcode/Remove_Duplicates_from_Sorted_Array_26.java
@@ -1,7 +1,11 @@
 package leetcode;
 
 import java.util.Arrays;
-
+/**
+ * 
+ * 去除数组中重复的元素
+ *
+ */
 public class Remove_Duplicates_from_Sorted_Array_26 {
 
 	public static void main(String[] args) {
@@ -9,9 +13,25 @@ public class Remove_Duplicates_from_Sorted_Array_26 {
 		System.out.println(removeDuplicates(input));
 	}
 	
+	//自己的解
 	 public static int removeDuplicates(int[] nums) {
 		    nums  = Arrays.stream(nums).distinct().sorted().toArray();
 	        return nums.length;
-	    }
+	  }
+	 
+	 //leetcode的解
+	 public static int removeDuplicates2(int[] A) {
+		 int j=0;
+         if (A.length==0) {
+        	 return 0; 
+         }
+        
+         for (int i=0; i<A.length; i++) {
+        	 if (A[i]!=A[j]) {
+        		 A[++j]=A[i];
+        	 }
+         }
+         return ++j;
+}
 
 }

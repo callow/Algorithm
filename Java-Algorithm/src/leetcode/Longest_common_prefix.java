8ca83bc7c0382301c455802bcd7c2722bb859109
Songlei0704 <Songlei0704@14087-NB.cse.com.sg>
2017-08-03 11:30:11 +0800

    update

---
+++ Java-Algorithm/src/leetcode/Longest_common_prefix.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * 	求所有字符串的最长公共前缀，即数组的所有字符串都包含这个前缀
+ * 
+ *
+ */
+public class Longest_common_prefix {
+
+	public static void main(String[] args) {
+		String[] input = {"ca", "cc", "ccac", "cccq", "ccd"};
+		System.out.println(longestCommonPrefix(input));
+	}
+	
+	public static String longestCommonPrefix(String[] strs) {
+	    if(strs == null || strs.length == 0)    return "";
+	    String pre = strs[0];
+	    int i = 1;
+	    while(i < strs.length) { // 遍历所有元素,从第二个开始遍历
+	        while(strs[i].indexOf(pre) != 0) { // 如果 pre 不是位于其他元素的前缀位置
+	            pre = pre.substring(0,pre.length()-1); // 则  pre 要变化
+	        }
+	        i++; // 下一个元素
+	    }
+	    return pre;
+	}
+
+}

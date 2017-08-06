ab1a7187cb916711994d6bc66d9c702e28511a7d
Nanyi <Nanyi@Znanyi.home>
2017-08-06 08:10:11 +0800

    add some comment

--- Java-Algorithm/src/leetcode/Remove_Element_27.java
+++ Java-Algorithm/src/leetcode/Remove_Element_27.java
@@ -15,7 +15,7 @@ public class Remove_Element_27 {
 		
 		
 	}
-	
+	 // 我的解决
 	 public static int removeElement(int[] nums, int val) {
 		    int i = 0;
 		    for (int j = 0; j < nums.length; j++) {
@@ -29,5 +29,21 @@ public class Remove_Element_27 {
 		    
 		    return i;
 	 }
+	 
+	 // lettcode的解，就是一个排序算法
+	 public static int removeElement2(int[] nums, int val) {
+	        int j = 0; int count = 0;
+	        for (int i = 0; i < nums.length; i++) {
+	            if (nums[i] != val) {
+	                int tmp = nums[j];
+	                nums[j] = nums[i];
+	                nums[i] = tmp;
+	                count++;
+	                j++;
+	            }
+	            
+	        }
+	        return count;
+	    }
 
 }

516dd69f92a4404cea1bbba632dffd2e918a6cd6
Nanyi <Nanyi@Znanyi.home>
2017-08-06 08:15:22 +0800

    add

---
+++ Java-Algorithm/src/leetcode/Implement_strStr_28.java
@@ -0,0 +1,11 @@
+package leetcode;
+
+public class Implement_strStr_28 {
+	public static void main(String[] args) {
+		System.out.println(strStr("as","a"));
+	}
+	
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}

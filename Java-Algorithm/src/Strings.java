36ee99ae465624af6e95c680a0b4a8a2e543a52c
songlei@duotechglobal.com <songlei@duotechglobal.com>
2021-06-16 20:39:19 +0800

    update

---
+++ Java-Algorithm/src/Strings.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Strings {
+
+	public boolean generateDocument(String characters, String document) {
+		String[] chars = characters.split("");
+		String[] docs = document.split("");
+		if (document.equals("")) {
+			return true;
+		}
+		Map<String, Integer> charMap = new HashMap<>();
+		for (String s : chars) {
+			if (charMap.containsKey(s)) {
+				charMap.put(s, charMap.get(s) + 1);
+			} else {
+				charMap.put(s, 1);
+			}
+		}
+		for (String s : docs) {
+			if (charMap.containsKey(s)) {
+				charMap.put(s, charMap.get(s) - 1);
+			}
+			if (!charMap.containsKey(s) || charMap.containsValue(-1)) {
+				return false;
+			}
+		}
+		return true;
+	}
+}

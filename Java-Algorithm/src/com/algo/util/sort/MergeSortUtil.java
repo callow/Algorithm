ba2e8a4b8cb571e1f5a8bf5f1a110630c2220d3f
Song Lei <Lei.Song@duotechglobal.com>
2022-11-05 15:11:07 +0800

    update

---
+++ Java-Algorithm/src/com/algo/util/sort/MergeSortUtil.java
@@ -0,0 +1,159 @@
+package com.algo.util.sort;
+/**
+ * MergeSort 是分而治之思路， 砍一半， （左边总个数 + 右边总个数 + Merge过程中的总个数）
+ * 
+ * 关键代码都是在Merge，它把很多东西变成有序的，间接结出很多题目
+ *
+ */
+
+public class MergeSortUtil {
+
+	/**
+	 * 数组每个位置左边只要比它小的数累加，不停合并，最终形成的Sum O（nLog(n)）<br>
+	 *  - 每一个数左边有多少个数比它小 = 每一个数右边多少个数比它大
+	 *  - 从左往右Merge
+	 */
+	
+	public static int smallSum(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return 0;
+		}
+		return processSmallSum(arr, 0, arr.length - 1);
+	}
+	
+	// arr[L..R]既要排好序，也要求小和返回
+	// 所有merge时，产生的小和，累加
+	// 左边排序   merge并产生小合
+	// 右边排序  merge并产生小合
+	private static int processSmallSum(int[] arr, int l, int r) {
+		if (l == r) {
+			return 0;
+		}
+		int mid = l + ((r - l) / 2);
+		// 最终的量 = 左边排序时产生的小合总量 + 右边排序时产生的小合总量 + Merge时产生的小合总量
+		return processSmallSum(arr, l, mid) + processSmallSum(arr, mid, r) + mergeSmallSum(arr, l, mid, r);
+	}
+	private static int mergeSmallSum(int[] arr, int l, int m, int r) {
+		int[] help = new int[r - l + 1];
+		int i = 0;
+		int p1 = l;
+		int p2 = m + 1;
+		int smallSum = 0;
+		while (p1 <= m && p2 <= r) {
+			// 左边 < 右边 时， 产生小合 (r - p2 + 1) * 左边那个数
+			// (r - p2 + 1) 右组中有几个数比当前数大
+			smallSum += arr[p1] < arr[p2] ? (r - p2 + 1) * arr[p1] : 0;
+			help[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
+		}
+		while (p1 <= m) {
+			help[i++] = arr[p1++];
+		}
+		while (p2 <= r) {
+			help[i++] = arr[p2++];
+		}
+		for (i = 0; i < help.length; i++) {
+			arr[l + i] = help[i];
+		}
+		return smallSum;
+	}
+	
+	/**
+	 * 逆序对： 左>右，不需要连续，	数组有几个逆序对？  O（nLog(n)） <br>
+	 *  - 每一个数右边有多少个数比它小的 = 反小合问题
+	 *  - 从右往左Merge!
+	 */
+	
+	public static int countReversePair(int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return 0;
+		}
+		return processCountRevPair(arr, 0, arr.length - 1);
+	}
+	
+	// arr[L..R]既要排好序，也要求逆序对数量返回
+	// 所有merge时，产生的逆序对数量，累加，返回
+	// 左 排序 merge并产生逆序对数量
+	// 右 排序 merge并产生逆序对数量
+	private static int processCountRevPair(int[] arr, int l, int r) {
+		if (l == r) {
+			return 0;
+		}
+		int mid = l + ((r - l) / 2);
+		// 左边你给我你的count + 右边你给我你的count + 合并起来给我一下count
+		return processCountRevPair(arr, l, mid) + processCountRevPair(arr, mid, r) + mergeCountRevPair(arr, l, mid, r);
+	}
+	
+	private static int mergeCountRevPair(int[] arr, int l, int m, int r) {
+		int[] help = new int[r - l + 1];
+		int i = help.length - 1;
+		int p1 = m; // mid
+		int p2 = r; // rightest
+		int counter = 0;
+		while (p1 >= l && p2 > m) {
+			counter += arr[p1] > arr[p2] ? (p2 - m) : 0;
+			help[i--] = arr[p1] > arr[p2] ? arr[p1--] : arr[p2--];
+		}
+		while (p1 >= l) {
+			help[i--] = arr[p1--];
+		}
+		while (p2 > m) {
+			help[i--] = arr[p2--];
+		}
+		for (i = 0; i < help.length; i++) {
+			arr[l + i] = help[i];
+		}
+		return counter;
+	}
+	
+	/**
+	 * Great num => 它 右边有几个数 * 2 都没有它大。 数一下总数？ <br>
+	 *  - 如果左右都是有序的，则指针可以不回退
+	 *  - https://leetcode.com/problems/reverse-pairs/
+	 */
+	public static int countGreatNum (int[] arr) {
+		if (arr == null || arr.length < 2) {
+			return 0;
+		}
+		return processCountGreatNum(arr, 0, arr.length - 1);
+	}
+	
+	private static int processCountGreatNum(int[] arr, int l, int r) {
+		if (l == r) {
+			return 0;
+		}
+		int mid = l + ((r - l) / 2);
+		return processCountGreatNum(arr, l, mid) + processCountGreatNum(arr, mid, r) + mergeCountGreatNum(arr, l, mid, r);
+	}
+	
+	public static int mergeCountGreatNum(int[] arr, int L, int m, int r) {
+		// 左组： [L....M] 右组： [M+1....R]
+		int ans = 0;
+		// 目前囊括进来的数，是从[M+1, windowR)
+		int windowR = m + 1;
+		for (int i = L; i <= m; i++) {
+			while (windowR <= r && (long) arr[i] > (long) arr[windowR] * 2) {
+				windowR++;
+			}
+			ans += windowR - m - 1;
+		}
+		int[] help = new int[r - L + 1];
+		int i = 0;
+		int p1 = L;
+		int p2 = m + 1;
+		while (p1 <= m && p2 <= r) {
+			help[i++] = arr[p1] <= arr[p2] ? arr[p1++] : arr[p2++];
+		}
+		while (p1 <= m) {
+			help[i++] = arr[p1++];
+		}
+		while (p2 <= r) {
+			help[i++] = arr[p2++];
+		}
+		for (i = 0; i < help.length; i++) {
+			arr[L + i] = help[i];
+		}
+		return ans;
+	}
+	
+	
+}

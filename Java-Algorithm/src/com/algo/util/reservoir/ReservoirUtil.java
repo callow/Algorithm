31961adfd74a2278659fe59c5273fc6662186ead
Song Lei <Lei.Song@duotechglobal.com>
2022-12-05 14:04:49 +0800

    d

--- Java-Algorithm/src/com/algo/util/reservoir/ReservoirUtil.java
+++ Java-Algorithm/src/com/algo/util/reservoir/ReservoirUtil.java
@@ -8,12 +8,17 @@ import com.algo.util.reservoir.model.Reservoir;
  */
 
 public class ReservoirUtil {
-
-	/**
-	 * 全球用户，首次登录都有一次获奖机会抽前100名获奖者，做到不用存储，且流式计算得奖者
-	 */
 	
 	private static Reservoir POOL = new Reservoir(100);
+
+	/**
+	 * 全球用户，首次登录都有一次获奖机会抽前100名获奖者，做到不用存储，且流式计算得奖者。<br>
+	 * 
+	 * 得奖必须公平 / 即等概率！ <br><br>
+	 * 
+	 * 则蓄水池在插入的时候就是等概率，刚好符合
+	 */
+	
 	public static int[] lottery() {
 		
 		// 假设全球总共有10000个用户，加入蓄水池

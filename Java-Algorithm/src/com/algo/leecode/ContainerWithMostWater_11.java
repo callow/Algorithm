6ccf521f56dff5788cedfbf4bc4dd9e6025ff53b
Song Lei <Lei.Song@duotechglobal.com>
2022-12-11 13:21:56 +0800

    a

---
+++ Java-Algorithm/src/com/algo/leecode/ContainerWithMostWater_11.java
@@ -0,0 +1,10 @@
+package com.algo.leecode;
+
+import com.algo.competition.w8_bracket_water_matrix_snake.WaterContainer;
+
+public class ContainerWithMostWater_11 {
+
+	public static void main(String[] args) {
+		System.out.println(WaterContainer.class);
+	}
+}

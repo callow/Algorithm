2c5cb2a9aec7bfa6e5017dd94f89434a13fa72f4
Song Lei <songlei198974@gmail.com>
2023-03-19 23:03:56 +0800

    d

--- Java-Algorithm/src/com/algo/util/dp/DPUtil.java
+++ Java-Algorithm/src/com/algo/util/dp/DPUtil.java
@@ -1,8 +1,10 @@
 package com.algo.util.dp;
 
 import java.util.List;
+import java.util.Stack;
 
 import com.algo.util.dp.impl.DPRecursive;
+import com.algo.util.stack.StackUtil;
 
 public class DPUtil {
 	
@@ -30,6 +32,20 @@ public class DPUtil {
 	public static void printPermutation(String s) {
 		List<String> answers =  new DPRecursive().permutation(s);
 		answers.stream().forEach(System.out::println);
+	}
+	
+	/**
+	 * 逆序栈，但是不能用额外的数据结构
+	 */
+	
+	public static void reverseStack(Stack<Integer> stack) {
+		if (stack.isEmpty()) {
+			return;
+		}
+		int i = StackUtil.popBottom(stack);
+		reverseStack(stack);
+		stack.push(i);
+		StackUtil.popBottom(stack);
 	}
 	
 	
--- Java-Algorithm/src/com/algo/util/stack/StackUtil.java
+++ Java-Algorithm/src/com/algo/util/stack/StackUtil.java
@@ -1,5 +1,7 @@
 package com.algo.util.stack;
 
+import java.util.Stack;
+
 import com.algo.util.stack.model.Queue;
 
 public class StackUtil {
@@ -10,5 +12,21 @@ public class StackUtil {
 	
 	public static Queue<Integer> getQueue() {
 		return new Queue<>();
+	}
+	
+
+	/**
+	 * 栈底元素移除掉, 上面的元素盖下来 返回移除掉的栈底元素
+	 */
+	
+	public static int popBottom(Stack<Integer> stack) {
+		int result = stack.pop();
+		if (stack.isEmpty()) {
+			return result;
+		} else {
+			int last = popBottom(stack);
+			stack.push(result);
+			return last;
+		}
 	}
 }

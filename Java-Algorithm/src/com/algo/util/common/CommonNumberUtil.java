f6817f34624b3a40697bbef878dd680ef9ea47d2
Song Lei <songlei198974@gmail.com>
2023-03-30 06:58:11 +0800

    a

--- Java-Algorithm/src/com/algo/util/common/CommonNumberUtil.java
+++ Java-Algorithm/src/com/algo/util/common/CommonNumberUtil.java
@@ -4,6 +4,7 @@ public class CommonNumberUtil {
 
 	/**
 	 * 提取某个数字x第d位上的数，e.g 199 提取百位(2ed)上的数为9
+	 * 
 	 * @param x
 	 * @param d
 	 * @return
@@ -11,11 +12,11 @@ public class CommonNumberUtil {
 	public static int getDigit(int x, int d) {
 		return ((x / ((int) Math.pow(10, d - 1))) % 10);
 	}
-	
+
 	/**
-	 * 数组中最大数有多少位 
+	 * 数组中最大数有多少位
 	 */
-	
+
 	public static int maxbits(int[] arr) {
 		int max = Integer.MIN_VALUE;
 		for (int i = 0; i < arr.length; i++) {
@@ -28,19 +29,34 @@ public class CommonNumberUtil {
 		}
 		return res;
 	}
-	
+
 	/**
 	 * 
 	 * 数字有几位
 	 */
-	
+
 	public static int countDigits(int num) {
-	    int count = 0;
-	    while (num != 0) {
-	      num /= 10;
-	      ++count;
-	    }
-	    return count;
+		int count = 0;
+		while (num != 0) {
+			num /= 10;
+			++count;
+		}
+		return count;
 	}
-		
+
+	/**
+	 * 
+	 * 将数组中的0重复一次，数组元素依次右移，且不可以扩容
+	 */
+	public static void duplicateZeros(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == 0 && i != arr.length - 1) {
+				for (int j = arr.length - 1; j > i; j--) {
+					arr[j] = arr[j - 1];
+				}
+				i++;
+			}
+		}
+	}
+
 }

e5d83ef90e366b72478add2cc2d9285422e71642
Song Lei <songlei198974@gmail.com>
2023-04-04 06:58:57 +0800

    a

--- Java-Algorithm/src/com/algo/util/array/ArrayUtil.java
+++ Java-Algorithm/src/com/algo/util/array/ArrayUtil.java
@@ -11,11 +11,11 @@ public class ArrayUtil {
 	/**
 	 * 生成一个数组实现的Queue
 	 */
-	
+
 	public static Queue<Integer> getQueue() {
 		return new Queue<Integer>(100000000);
 	}
-	
+
 	/**
 	 * 合并互相覆盖的区间<br>
 	 * intervals = [[1,3],[2,6],[8,10],[15,18]] -> [[1,6],[8,10],[15,18]]
@@ -25,81 +25,114 @@ public class ArrayUtil {
 		if (intervals == null || intervals.length == 0) {
 			return intervals;
 		}
-		Arrays.sort(intervals, (a,b) -> (a[0] - b[0])); // 从小到大排序
-		
+		Arrays.sort(intervals, (a, b) -> (a[0] - b[0])); // 从小到大排序
+
 		int[] cur = intervals[0];
-		for(int i = 1; i < intervals.length; i++) {
+		for (int i = 1; i < intervals.length; i++) {
 			if (intervals[i][0] > cur[1]) { // 无重叠
 				result.add(cur);
 				cur = intervals[i];
 			} else {
-				cur[1] = Math.max(cur[1], intervals[i][1]);  // 有重叠 可combine, 更新end 推更高
+				cur[1] = Math.max(cur[1], intervals[i][1]); // 有重叠 可combine, 更新end 推更高
 			}
 		}
 		result.add(cur);
 		return result.toArray(new int[result.size()][2]); // 将ArrayList变成数组的方法
 	}
-	
+
 	/**
 	 * 找到最长的连续的1的长度
 	 */
-	
-    public static int findMaxConsecutiveOnes(int[] nums) {
-    	int count = 0;
-    	int max = 0;
-    	for(int i = 0; i < nums.length; i++) {
-        	if (nums[i] == 1) {
-        		count++;
-        	} else {
-        		max = Math.max(max, count);
-        		count = 0;
-        	}
-    	}
-    	return Math.max(max, count);
-    }
-    
-    /**
-     * 检查数组中是否有元素是我的2倍 
-     */
-    public static boolean hasElement2TimesThanMe(int[] arr) {
-       List<Integer> copy = new ArrayList<>();
- 	   for (int i =0; i < arr.length; i++) {
- 		   copy.add(arr[i] * 2);
- 	   }
- 	   
- 	   for (int i =0; i < arr.length; i++) {
- 		   if (copy.contains(arr[i]) && i != copy.indexOf(arr[i])) {
- 			   return true;
- 		   }
- 	   }
- 	   return false;
-    }
-    
-    /**
-     * 是山形数组 
-     */
-    
-    public static boolean isMountainArray(int[] arr) {
+
+	public static int findMaxConsecutiveOnes(int[] nums) {
+		int count = 0;
+		int max = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == 1) {
+				count++;
+			} else {
+				max = Math.max(max, count);
+				count = 0;
+			}
+		}
+		return Math.max(max, count);
+	}
+
+	/**
+	 * 检查数组中是否有元素是我的2倍
+	 */
+	public static boolean hasElement2TimesThanMe(int[] arr) {
+		List<Integer> copy = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++) {
+			copy.add(arr[i] * 2);
+		}
+
+		for (int i = 0; i < arr.length; i++) {
+			if (copy.contains(arr[i]) && i != copy.indexOf(arr[i])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 是山形数组
+	 */
+
+	public static boolean isMountainArray(int[] arr) {
 		if (arr.length < 3) {
 			return false;
 		}
 		int turningPoint = -1;
-        for (int i = 1; i < arr.length; i++) {
-        	if (arr[i] == arr[i-1]) {
-        		return false;
-        	}
-        	if (turningPoint != -1 && arr[i] > arr[i-1]) {
-        		return false;
-        	}
-        	
-        	if (arr[i] < arr[i-1]) {
-        		turningPoint = i-1;
-        	}
-        	if (turningPoint == 0) {
-        		return false;
-        	}
-        }
-        return turningPoint == -1 ? false : true;
-    }
-	
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] == arr[i - 1]) {
+				return false;
+			}
+			if (turningPoint != -1 && arr[i] > arr[i - 1]) {
+				return false;
+			}
+
+			if (arr[i] < arr[i - 1]) {
+				turningPoint = i - 1;
+			}
+			if (turningPoint == 0) {
+				return false;
+			}
+		}
+		return turningPoint == -1 ? false : true;
+	}
+
+	/**
+	 * 删除连续的重复元素
+	 */
+	public static int removeDuplicates(int[] nums) {
+		int insertIndex = 1;
+		for (int i = 1; i < nums.length; i++) {
+			if (nums[i - 1] != nums[i]) {
+				nums[insertIndex] = nums[i];
+				insertIndex++;
+			}
+		}
+		return insertIndex;
+	}
+
+	/**
+	 * 删除某个数字
+	 */
+	public static int removeElement(int[] nums, int val) {
+		int counter = 0;
+		if (nums.length != 0) {
+			int[] nNums = new int[nums.length];
+			for (int i = 0; i < nums.length; i++) {
+				if (nums[i] != val) {
+					nNums[counter++] = nums[i];
+				}
+			}
+			if (nNums.length != 0) {
+				System.arraycopy(nNums, 0, nums, 0, nNums.length);
+			}
+		}
+		return counter;
+	}
+
 }

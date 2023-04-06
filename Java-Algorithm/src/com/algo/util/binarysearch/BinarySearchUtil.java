d381af9175c26541f073c7d5e40f3a236f162813
Song Lei <songlei198974@gmail.com>
2023-04-06 08:23:52 +0800

    a

--- Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
+++ Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
@@ -3,11 +3,11 @@ package com.algo.util.binarysearch;
 import com.algo.util.common.CommonArrayUtil;
 
 public class BinarySearchUtil {
-	
+
 	/**
 	 * 二分查找 O(Log(N))
 	 */
-	
+
 	public static boolean exist(int[] sortArr, int num) {
 		if (CommonArrayUtil.isEmpty(sortArr)) {
 			return false;
@@ -15,34 +15,34 @@ public class BinarySearchUtil {
 		int l = 0;
 		int r = sortArr.length - 1;
 		int mid = 0;
-		
+
 		while (l < r) {
-			mid  = l + ((r-l)/2);
+			mid = l + ((r - l) / 2);
 			if (sortArr[mid] == num) {
 				return true;
 			} else if (sortArr[mid] > num) {
-				r = mid -1;
+				r = mid - 1;
 			} else {
 				l = mid + 1;
 			}
 		}
 		return sortArr[l] == num;
 	}
-	
+
 	/**
 	 * 二分查找 >= num 最左位置. O(Log(N))
 	 */
-	
+
 	public static int nearestLeftIndex(int[] sortArr, int num) {
 		int l = 0;
-		int r = sortArr.length -1;
+		int r = sortArr.length - 1;
 		int mid = 0;
-		
+
 		int answer = -1;
-		while(l <= r) {
-			mid  = l + ((r-l)/2);
+		while (l <= r) {
+			mid = l + ((r - l) / 2);
 			if (sortArr[mid] >= num) {
-				answer  = mid;
+				answer = mid;
 				r = mid - 1;
 			} else {
 				l = mid + 1;
@@ -50,19 +50,19 @@ public class BinarySearchUtil {
 		}
 		return answer;
 	}
-	
+
 	/**
 	 * 二分查找 <= num 最右位置. O(Log(N))
 	 */
-	
+
 	public static int nearestRightIndex(int[] sortArr, int num) {
 		int l = 0;
 		int r = sortArr.length - 1;
 		int mid = 0;
-		
+
 		int answer = -1;
-		while(l <= r) {
-			mid  = l + ((r-l)/2);
+		while (l <= r) {
+			mid = l + ((r - l) / 2);
 			if (sortArr[mid] <= num) {
 				answer = mid;
 				l = mid + 1;
@@ -72,30 +72,30 @@ public class BinarySearchUtil {
 		}
 		return answer;
 	}
-	
+
 	/**
 	 * 二分查找： 无序数组中找一个局部最小 e.g （19,1,4） -> 1. O(Log(N))<br>
-	 *   - 先去2头找局部最小， 2头没有再去中间找 <br>
+	 * - 先去2头找局部最小， 2头没有再去中间找 <br>
 	 */
-	
+
 	public static int localMinimum(int[] unsortArr) {
 		if (CommonArrayUtil.isEmpty(unsortArr)) {
 			return -1;
 		}
 		// 去头找
 		if (CommonArrayUtil.hasOne(unsortArr) || unsortArr[0] < unsortArr[1]) {
-			return 0; 
+			return 0;
 		}
 		// 去尾找
 		if (unsortArr[unsortArr.length - 1] < unsortArr[unsortArr.length - 2]) {
 			return unsortArr.length - 1;
 		}
-		
+
 		// 去中间找
 		int l = 1;
 		int r = unsortArr.length - 2;
 		int mid = 0;
-		
+
 		while (l < r) {
 			mid = (l + r) / 2;
 			if (unsortArr[mid] > unsortArr[mid - 1]) { // 1 < 9（mid）
@@ -107,5 +107,37 @@ public class BinarySearchUtil {
 			}
 		}
 		return l;
+	}
+
+	/**
+	 * 猜数字游戏：大了，小了 对了！
+	 */
+	public static int guessNumber(int num) {
+		int l = 1;
+		int r = Integer.MAX_VALUE - 1;
+		int mid = 0;
+
+		while (l < r) {
+			mid = l + ((r - l) / 2);
+			if (guess(mid) == 0) {
+				return mid;
+			} else if (guess(mid) < 0) {
+				r = mid - 1;
+			} else {
+				l = mid + 1;
+			}
+		}
+		return l;
+	}
+
+	private static int guess(int myguess) {
+		int answer = 6;
+		if (myguess == answer) {
+			return 0;
+		} else if (myguess > answer) {
+			return -1;
+		} else {
+			return 1;
+		}
 	}
 }

9182760712bbd9c9eedfdf34ffd00f24fce73b73
Song Lei <songlei198974@gmail.com>
2023-04-08 09:33:45 +0800

    a

--- Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
+++ Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
@@ -140,4 +140,37 @@ public class BinarySearchUtil {
 			return 1;
 		}
 	}
+
+	/**
+	 * 在旋转数组中查找某数
+	 */
+	public static int searchInRotatedArray(int[] arr, int num) {
+		if (CommonArrayUtil.isEmpty(arr)) {
+			return -1;
+		}
+		int l = 0;
+		int r = arr.length - 1;
+		int mid = 0;
+
+		while (l <= r) {
+			mid = l + ((r - l) / 2);
+			if (arr[mid] == num) {
+				return mid;
+			} else if (arr[mid] > arr[l]) {
+				if (num >= arr[l] && num < arr[mid]) {
+					r = mid - 1;
+				} else {
+					l = mid + 1;
+				}
+			} else {
+				if (num <= arr[r] && num > arr[mid]) {
+					l = mid + 1;
+				} else
+					r = mid - 1;
+			}
+
+		}
+		return -1;
+	}
+
 }

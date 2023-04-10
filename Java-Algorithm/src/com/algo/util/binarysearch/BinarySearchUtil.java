94bf40658e4ea1c8da04615afc8a55464e7bcf5b
Song Lei <songlei198974@gmail.com>
2023-04-11 07:31:43 +0800

    a

--- Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
+++ Java-Algorithm/src/com/algo/util/binarysearch/BinarySearchUtil.java
@@ -232,4 +232,58 @@ public class BinarySearchUtil {
 		return Integer.MAX_VALUE;
 	}
 
+	/**
+	 * 搜索连续的上下边界
+	 */
+
+	public static int[] findRange(int[] nums, int target) {
+		int leftBound = findBound(nums, target, true);
+		if (leftBound == -1) {
+			return new int[] { -1, -1 };
+		}
+		int rightBound = findBound(nums, target, false);
+		return new int[] { leftBound, rightBound };
+	}
+
+	private static int findBound(int[] nums, int target, boolean isLeft) {
+		int l = 0;
+		int r = nums.length - 1;
+
+		while (l <= r) {
+
+			int mid = (l + r) / 2;
+
+			if (nums[mid] == target) {
+
+				if (isLeft) {
+
+					// This means we found our lower bound.
+					if (mid == l || nums[mid - 1] != target) {
+						return mid;
+					}
+
+					// Search on the left side for the bound.
+					r = mid - 1;
+
+				} else {
+
+					// This means we found our upper bound.
+					if (mid == r || nums[mid + 1] != target) {
+						return mid;
+					}
+
+					// Search on the right side for the bound.
+					l = mid + 1;
+				}
+
+			} else if (nums[mid] > target) {
+				r = mid - 1;
+			} else {
+				l = mid + 1;
+			}
+		}
+
+		return -1;
+	}
+
 }

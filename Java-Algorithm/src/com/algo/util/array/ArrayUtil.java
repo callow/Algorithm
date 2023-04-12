eac6a9d8ae629bde27d57ddb7f4a35c0475fefd8
Song Lei <songlei198974@gmail.com>
2023-04-13 07:52:48 +0800

    a

--- Java-Algorithm/src/com/algo/util/array/ArrayUtil.java
+++ Java-Algorithm/src/com/algo/util/array/ArrayUtil.java
@@ -175,4 +175,25 @@ public class ArrayUtil {
 		return counter < 3 ? answer.get(counter - 1) : answer.get(counter - 3);
 	}
 
+	/**
+	 * 给数组组成的整数+1，然后再返回数组,进位 -> [1,2,4] -> [1,2,5]
+	 */
+	public static int[] plusOne(int[] d) {
+		int[] digits = new int[d.length + 1];
+		System.arraycopy(d, 0, digits, 1, d.length);
+		digits[digits.length - 1]++;
+
+		for (int i = digits.length - 1; i > 0; i--) {
+			if (digits[i] == 10) {
+				digits[i] = 0;
+				digits[i - 1]++;
+			}
+		}
+		if (digits[0] == 0) {
+			System.arraycopy(digits, 1, d, 0, d.length);
+			return d;
+		}
+		return digits;
+	}
+
 }

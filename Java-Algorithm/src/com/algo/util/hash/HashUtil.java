97882c9d8f4925b78701541cfb2fadc5a9b5fd92
Song Lei <songlei198974@gmail.com>
2023-04-23 23:22:02 +0800

    a

--- Java-Algorithm/src/com/algo/util/hash/HashUtil.java
+++ Java-Algorithm/src/com/algo/util/hash/HashUtil.java
@@ -1,5 +1,6 @@
 package com.algo.util.hash;
 
+import java.math.BigInteger;
 import java.nio.charset.Charset;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -12,12 +13,11 @@ import javax.xml.bind.DatatypeConverter;
 import com.google.common.hash.Hashing;
 
 public class HashUtil {
-	
-	
+
 	/**
 	 * 获取JDK默认支持的Hash算法名
 	 */
-	
+
 	public static final Set<String> getJDKHashAlgorithms() {
 		Set<String> set = new HashSet<>();
 		for (String str : Security.getAlgorithms("MessageDigest")) {
@@ -25,20 +25,29 @@ public class HashUtil {
 		}
 		return set;
 	}
-	
+
 	/**
 	 * 使用特定算法获取某个字符串的HashCode, e.g: getHashCode('MD5','SONGLEI')
 	 */
-	
+
 	public static final String getHashCode(String algorithm, String input) throws NoSuchAlgorithmException {
 		MessageDigest hash = MessageDigest.getInstance(algorithm);
 		return DatatypeConverter.printHexBinary(hash.digest(input.getBytes())).toUpperCase();
 	}
-	
+
+	/**
+	 * 使用特定算法获取某个字符串的HashValue, e.g: getHashValue('MD5','SONGLEI')
+	 */
+
+	public static long getHashValue(String algorithm, String input) throws NoSuchAlgorithmException {
+		MessageDigest hash = MessageDigest.getInstance(algorithm);
+		return new BigInteger(hash.digest(input.getBytes())).longValue();
+	}
+
 	/**
 	 * 生成N种不同的HashValues,提供给BoomFilter : i * f1(hashvalue) + f2（hashvalue)
 	 */
-	
+
 	public Set<Long> generateNDiffHashcode(String input, int n) {
 		Set<Long> hashValues = new HashSet<>(n);
 		long f1 = Hashing.murmur3_128().hashString(input, Charset.defaultCharset()).asLong();

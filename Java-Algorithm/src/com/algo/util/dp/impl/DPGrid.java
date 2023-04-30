2e6041c59c7426caa65d4811dd8aff8004edb3a1
Song Lei <songlei198974@gmail.com>
2023-04-30 10:18:16 +0800

    a

--- Java-Algorithm/src/com/algo/util/dp/impl/DPGrid.java
+++ Java-Algorithm/src/com/algo/util/dp/impl/DPGrid.java
@@ -463,9 +463,54 @@ public class DPGrid implements DPService {
 		return dp[0][sum];
 	}
 
+	/**
+	 * 3个可变参数 是一个3维表
+	 */
 	@Override
 	public int splitArrSumSizeHalf(int[] arr) {
-		return 0;
+		if (CommonArrayUtil.isEmpty(arr) || CommonArrayUtil.hasOne(arr)) {
+			return 0;
+		}
+		int sum = 0;
+		for (int num : arr) {
+			sum += num;
+		}
+		sum /= 2;
+		int N = arr.length;
+		int M = (N + 1) / 2;
+		int[][][] dp = new int[N + 1][M + 1][sum + 1];
+		for (int i = 0; i <= N; i++) {
+			for (int j = 0; j <= M; j++) {
+				for (int k = 0; k <= sum; k++) {
+					dp[i][j][k] = -1;
+				}
+			}
+		}
+		for (int rest = 0; rest <= sum; rest++) {
+			dp[N][0][rest] = 0;
+		}
+		for (int i = N - 1; i >= 0; i--) {
+			for (int picks = 0; picks <= M; picks++) {
+				for (int rest = 0; rest <= sum; rest++) {
+					int p1 = dp[i + 1][picks][rest];
+					// 就是要使用arr[i]这个数
+					int p2 = -1;
+					int next = -1;
+					if (picks - 1 >= 0 && arr[i] <= rest) {
+						next = dp[i + 1][picks - 1][rest - arr[i]];
+					}
+					if (next != -1) {
+						p2 = arr[i] + next;
+					}
+					dp[i][picks][rest] = Math.max(p1, p2);
+				}
+			}
+		}
+		if ((arr.length & 1) == 0) {
+			return dp[0][arr.length / 2][sum];
+		} else {
+			return Math.max(dp[0][arr.length / 2][sum], dp[0][(arr.length / 2) + 1][sum]);
+		}
 	}
 
 }

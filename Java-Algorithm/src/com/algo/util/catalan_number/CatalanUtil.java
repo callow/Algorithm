fe03cd00f04755e44db5ee3e3106a8ff67a7c7ce
Song Lei <songlei198974@gmail.com>
2023-05-09 11:07:12 +0800

    A

---
+++ Java-Algorithm/src/com/algo/util/catalan_number/CatalanUtil.java
@@ -0,0 +1,12 @@
+package com.algo.util.catalan_number;
+
+/**
+ * 卡特兰数：组合数学种的数列<br>
+ * 3个通项公式：已知 k(0) = 0, k(1) = 1<br>
+ * - k(n) = k(0)*k(n-1) + k(1)*k(n-2) + .... + k(n-1)*k(0)<br>
+ * - k(n) = c(2n,n) - c(2n,n-1)<br>
+ * - k(n) = c(2n,n) / n+1
+ */
+public class CatalanUtil {
+
+}

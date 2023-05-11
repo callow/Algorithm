01c7df66b387f0478ad9754a45f8c82c05711d27
Song Lei <Lei.Song@duotechglobal.com>
2023-05-11 14:30:41 +0800

    a

--- Java-Algorithm/src/com/algo/pattern/decorator/DecoratorUtil.java
+++ Java-Algorithm/src/com/algo/pattern/decorator/DecoratorUtil.java
@@ -8,6 +8,8 @@ import com.algo.pattern.decorator.model.ReadingMaterials;
 /**
  * 
  * 动态地给一个对象添加一些额外的职责。就增加功能来说，装饰器模式相比生成子类更为灵活。 = Wrapper
+ * 
+ * 这种设计模式通过子类的灵活拼装，实现了各种属性随意组合的功能
  *
  */
 public class DecoratorUtil {

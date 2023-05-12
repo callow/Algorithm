0adecb0888e2c4cc8a0eed329cfce45cdf9825de
Song Lei <songlei198974@gmail.com>
2023-05-12 16:18:28 +0800

    a

---
+++ Java-Algorithm/src/com/algo/util/array_sum/ArraySumUtil.java
@@ -0,0 +1,40 @@
+package com.algo.util.array_sum;
+
+import com.algo.util.common.CommonArrayUtil;
+
+/**
+ * 
+ * 数组累加和相关问题。
+ *
+ */
+public class ArraySumUtil {
+
+	/**
+	 * 哪个子数组∑ = target且长度最长？<br>
+	 * 解：双指针, R++, L++
+	 */
+	public static int findLongestSubArray(int[] arr, int target) {
+		if (CommonArrayUtil.isEmpty(arr) || target <= 0) {
+			return 0;
+		}
+		int left = 0;
+		int right = 0;
+		int sum = arr[0];
+		int len = 0;
+		while (right < arr.length) {
+			if (sum == target) {
+				len = Math.max(len, right - left + 1);
+				sum -= arr[left++];
+			} else if (sum < target) {
+				right++;
+				if (right == arr.length) {
+					break;
+				}
+				sum += arr[right];
+			} else {
+				sum -= arr[left++];
+			}
+		}
+		return len;
+	}
+}

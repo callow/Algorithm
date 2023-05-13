d764e1e184c6c8e5d68c6bdf203205f3209b9906
Song Lei <songlei198974@gmail.com>
2023-05-14 07:36:31 +0800

    a

--- Java-Algorithm/src/com/algo/util/bitmask_dp/StateCompressionUtil.java
+++ Java-Algorithm/src/com/algo/util/bitmask_dp/StateCompressionUtil.java
@@ -2,7 +2,7 @@ package com.algo.util.bitmask_dp;
 
 /**
  * 
- * 状态压缩的动态规划
+ * 状态压缩的动态规划: 利用位运算来压缩空间
  *
  */
 public class StateCompressionUtil {
--- Java-Algorithm/src/com/algo/util/convexhull_dp/ConvexHullOptimisationUtil.java
+++ Java-Algorithm/src/com/algo/util/convexhull_dp/ConvexHullOptimisationUtil.java
@@ -2,7 +2,7 @@ package com.algo.util.convexhull_dp;
 
 /**
  * 
- * 斜率优化 = 四边形不等式优化的动态规划
+ * 斜率优化 = 四边形不等式优化的动态规划 ： 一个O(N^3)的优化掉最内侧的N -> O(N^2)
  *
  */
 public class ConvexHullOptimisationUtil {

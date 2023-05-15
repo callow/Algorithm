ecd0c2563e2d431d3fcc593352867adf153384b5
Song Lei <songlei198974@gmail.com>
2023-05-16 07:51:06 +0800

    a

--- Java-Algorithm/src/com/algo/util/array_sum/ArraySumUtil.java
+++ Java-Algorithm/src/com/algo/util/array_sum/ArraySumUtil.java
@@ -7,7 +7,7 @@ import com.algo.util.common.CommonArrayUtil;
 
 /**
  * 
- * 数组累加和相关问题。
+ * 数组累加和相关问题。= 数组3连问题
  *
  */
 public class ArraySumUtil {
@@ -79,11 +79,58 @@ public class ArraySumUtil {
 	}
 
 	/**
-	 * 一个+ - 0 数组，哪个子数组∑ <= k且长度最长？
+	 * 一个+ - 0 数组，哪个子数组∑ <= k且长度最长？<br>
+	 * 
+	 * 解：生成2个辅助数组成对使用,从后往前填：<br>
+	 * 
+	 * minSum[] : 子数字必须以i开头，取得的最小的累加和 <br>
+	 * minSumEnd[] : 子数字必须以i开头子数组，当取得最小累加和时的右边界<br>
+	 * 
+	 * 思路： <br>
+	 * 1. 如果以i开头最小累加和都>k, 那么从0开头任何子数组累加和不可能<=k <br>
+	 * 2. 如果以i开头最小累加和<k, 则minSum继续右扩，直到>k停，存储答案<br>
+	 * e.g : 0 开头往右在minSum上扩，直到13位置超过K了停 长度14个，这时候换成1开头，看看14位置能不能进来，继续扩，然后2位置...
+	 * 
 	 *
 	 */
 	public static int findLongestSubArrayAnySymbolLessThanK(int[] arr, int k) {
-		return k;
+		if (arr == null || arr.length == 0) {
+			return 0;
+		}
+		int[] minSums = new int[arr.length];
+		int[] minSumEnds = new int[arr.length];
+		minSums[arr.length - 1] = arr[arr.length - 1];
+		minSumEnds[arr.length - 1] = arr.length - 1;
+		for (int i = arr.length - 2; i >= 0; i--) {
+			if (minSums[i + 1] < 0) {
+				minSums[i] = arr[i] + minSums[i + 1];
+				minSumEnds[i] = minSumEnds[i + 1];
+			} else {
+				minSums[i] = arr[i];
+				minSumEnds[i] = i;
+			}
+		}
+
+		// 开始扩
+		int end = 0;// 迟迟扩不进来那一块儿的开头位置，0~13， 则end停在14位置
+		int sum = 0; // 窗口累加和
+		int ans = 0; // 最大长度/答案
+		for (int i = 0; i < arr.length; i++) {
+			// while循环结束之后：
+			// 1) 如果以i开头的情况下，累加和<=k的最长子数组是arr[i..end-1]，看看这个子数组长度能不能更新res；
+			// 2) 如果以i开头的情况下，累加和<=k的最长子数组比arr[i..end-1]短，更新还是不更新res都不会影响最终结果；
+			while (end < arr.length && sum + minSums[end] <= k) {
+				sum += minSums[end];
+				end = minSumEnds[end] + 1;
+			}
+			ans = Math.max(ans, end - i); // end 是第一个进不来的位置 - i现在开头的位置
+			if (end > i) { // 还有窗口，哪怕窗口没有数字 [i~end) [4,4)
+				sum -= arr[i];
+			} else { // i == end, 即将 i++, i > end, 此时窗口概念维持不住了，所以end跟着i一起走
+				end = i + 1;
+			}
+		}
+		return ans;
 
 	}
 

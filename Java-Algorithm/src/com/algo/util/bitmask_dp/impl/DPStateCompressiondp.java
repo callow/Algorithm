1034f7c4195578ed815de24aff51c092267c4dd3
Song Lei <songlei198974@gmail.com>
2023-05-17 07:45:23 +0800

    a

--- Java-Algorithm/src/com/algo/util/bitmask_dp/impl/DPStateCompressiondp.java
+++ Java-Algorithm/src/com/algo/util/bitmask_dp/impl/DPStateCompressiondp.java
@@ -6,7 +6,17 @@ public class DPStateCompressiondp implements StateCompressionDPService {
 
 	@Override
 	public boolean canWin(int choose, int total) {
-		return false;
+		if (total == 0) {
+			return true;
+		}
+		if ((choose * (choose + 1) >> 1) < total) {
+			return false;
+		}
+		int[] dp = new int[1 << (choose + 1)]; // dp表准备这么大，e.g: 11111 全1的状态是最大 所以就是准备的大小100000
+		// dp[status] == 1 true
+		// dp[status] == -1 false
+		// dp[status] == 0 process(status) 没算过！去算！
+		return pick(choose, 0, total, dp);
 	}
 
 }

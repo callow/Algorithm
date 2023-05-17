c8894d11e83e847c8395d7901b87a9566f29f9bf
Song Lei <songlei198974@gmail.com>
2023-05-18 07:43:37 +0800

    a

--- Java-Algorithm/src/com/algo/util/common/MatrixUtil.java
+++ Java-Algorithm/src/com/algo/util/common/MatrixUtil.java
@@ -240,7 +240,7 @@ public class MatrixUtil {
 	 *   .
 	 * c .
 	 * 
-	 *
+	 * 分圈结构！
 	 * 
 	 */
 	public static void rotate90Degree(int[][] matrix) {
@@ -264,4 +264,48 @@ public class MatrixUtil {
 			m[a + i][d] = tmp;
 		}
 	}
+	
+	/**
+	 * 
+	 * 分圈结构 ： 打印内螺旋小星星，每次留一个空格
+	 * 
+	 * 解：2层一个圈向内打印
+	 */
+	public static void printStar(int N) {
+		int leftUp = 0;
+		int rightDown = N - 1;
+		char[][] m = new char[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				m[i][j] = ' ';
+			}
+		}
+		while (leftUp <= rightDown) {
+			set(m, leftUp, rightDown);
+			leftUp += 2;
+			rightDown -= 2;
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(m[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void set(char[][] m, int leftUp, int rightDown) {
+		for (int col = leftUp; col <= rightDown; col++) {
+			m[leftUp][col] = '*';
+		}
+		for (int row = leftUp + 1; row <= rightDown; row++) {
+			m[row][rightDown] = '*';
+		}
+		for (int col = rightDown - 1; col > leftUp; col--) {
+			m[rightDown][col] = '*';
+		}
+		for (int row = rightDown - 1; row > leftUp + 1; row--) {
+			m[row][leftUp + 1] = '*';
+		}
+	}
+
 }

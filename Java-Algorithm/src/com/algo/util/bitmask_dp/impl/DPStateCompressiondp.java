e7196b49d547f3e76ef563330e4a561a5be13e77
Song Lei <songlei198974@gmail.com>
2023-05-18 07:50:42 +0800

    a

--- Java-Algorithm/src/com/algo/util/bitmask_dp/impl/DPStateCompressiondp.java
+++ Java-Algorithm/src/com/algo/util/bitmask_dp/impl/DPStateCompressiondp.java
@@ -4,6 +4,10 @@ import com.algo.util.bitmask_dp.StateCompressionDPService;
 
 public class DPStateCompressiondp implements StateCompressionDPService {
 
+	/**
+	 * status - > 2^N
+	 * O(N*2^N)
+	 */
 	@Override
 	public boolean canWin(int choose, int total) {
 		if (total == 0) {
@@ -17,6 +21,27 @@ public class DPStateCompressiondp implements StateCompressionDPService {
 		// dp[status] == -1 false
 		// dp[status] == 0 process(status) 没算过！去算！
 		return pick(choose, 0, total, dp);
+	}
+	
+	// 为什么明明status和rest是两个可变参数，却只用status来代表状态(也就是dp)
+	// 因为选了一批数字之后，得到的和一定是一样的，所以rest是由status决定的，所以rest不需要参与记忆化搜索
+	public static boolean pick(int choose, int status, int rest, int[] dp) {
+		if (dp[status] != 0) {
+			return dp[status] == 1 ? true : false;
+		}
+		boolean ans = false;
+		if (rest > 0) {
+			for (int i = 1; i <= choose; i++) {
+				if (((1 << i) & status) == 0) {
+					if (!pick(choose, (status | (1 << i)), rest - i, dp)) {
+						ans = true;
+						break;
+					}
+				}
+			}
+		}
+		dp[status] = ans ? 1 : -1;
+		return ans;
 	}
 
 }

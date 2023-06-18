14e2c852ddaebf2a6317bb5a4336c69e5e7b9c94
Song Lei <songlei198974@gmail.com>
2023-06-18 19:47:56 +0800

    a

---
+++ Java-Algorithm/src/com/algo/util/backtracking/BacktrackUtil.java
@@ -0,0 +1,35 @@
+package com.algo.util.backtracking;
+
+/**
+ * 
+ * backtracking 回溯 = 递归 ： 父过程调子过程<br>
+ * 
+ *  - 任何递归都可以改成非递归
+ *  - 递归何时结束 ： Base case
+ *	
+ */
+public class BacktrackUtil {
+
+	/**
+	 * 求Array中最大值： 故意用递归实现来理解
+	 */
+	public static int getArrayMax(int[] arr) {
+		return f(arr, 0, arr.length -1);
+	}
+	
+	private static int f(int[] arr, int L, int R) {
+		if (L == R) {
+			return arr[L];
+		}
+		int mid = L + (L+R) / 2;
+		
+		// 左侧的Max
+		int leftMax = f(arr,  L,  mid);
+		// 右侧的Max
+		int rightMax = f(arr,  mid,  R);
+		
+		// 比较一下
+		return Math.max(leftMax, rightMax);
+	}
+	
+}

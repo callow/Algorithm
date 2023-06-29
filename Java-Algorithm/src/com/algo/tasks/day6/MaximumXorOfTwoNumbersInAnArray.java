8c8aa61a9eb6e392cb38ea1ff39997fca6e91f79
Song Lei <songlei198974@gmail.com>
2023-06-29 08:16:08 +0800

    a

--- Java-Algorithm/src/com/algo/tasks/day6/MaximumXorOfTwoNumbersInAnArray.java
+++ Java-Algorithm/src/com/algo/tasks/day6/MaximumXorOfTwoNumbersInAnArray.java
@@ -14,11 +14,11 @@ public class MaximumXorOfTwoNumbersInAnArray {
 			return 0;
 		}
 		int max = Integer.MIN_VALUE;
-		MaxXOR.NumTrie numTrie = new MaxXOR.NumTrie(); // 可以和上一题公用一个前缀树
-		numTrie.add(arr[0]);
+		MaxXOR.NumTrie trie = new MaxXOR.NumTrie(); // 可以和上一题公用一个前缀树
+		trie.add(arr[0]);
 		for (int i = 1; i < arr.length; i++) {
-			max = Math.max(max, numTrie.maxXor(arr[i])); // 这题就不需要异或和了，直接在树上找带入即可
-			numTrie.add(arr[i]);
+			max = Math.max(max, trie.maxXor(arr[i])); // 这题就不需要异或和了，直接在树上找带入即可
+			trie.add(arr[i]);
 		}
 		return max;
 	}

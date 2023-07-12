4089ed2ae8db67765d3c46d0a24abe9f544901f0
Song Lei <songlei198974@gmail.com>
2023-07-13 06:56:21 +0800

    a

--- Java-Algorithm/src/com/algo/tasks/day8/FindWordInMatrix.java
+++ Java-Algorithm/src/com/algo/tasks/day8/FindWordInMatrix.java
@@ -1,5 +1,26 @@
 package com.algo.tasks.day8;
 
+/**
+	 * 给定一个char[][] matrix，也就是char类型的二维数组，再给定一个字符串word，
+	 * 可以从任何一个某个位置出发，可以走上下左右，能不能找到word？
+	 * 比如：
+	 * char[][] m = { 
+	 *     { 'a', 'b', 'z' }, 
+	 *     { 'c', 'd', 'o' }, 
+	 *     { 'f', 'e', 'o' }, 
+	 * };
+	 * word = "zooe"
+	 * 是可以找到的
+	 * 
+	 * 设定1：可以走重复路的情况下，返回能不能找到
+	 * 比如，word = "zoooz"，是可以找到的，z -> o -> o -> o -> z，因为允许走一条路径中已经走过的字符
+	 * 
+	 * 设定2：不可以走重复路的情况下，返回能不能找到
+	 * 比如，word = "zoooz"，是不可以找到的，因为允许走一条路径中已经走过的字符不能重复走
+	 * 
+	 * 写出两种设定下的code
+ * 
+ * */
 public class FindWordInMatrix {
 	
 	public static boolean findWordcanLoop(char[][] m, String word) {
@@ -86,6 +107,56 @@ public class FindWordInMatrix {
 		}
 		m[i][j] = original; // 恢复现场
 		return ans;
+	}
+	
+	
+	
+	
+	
+	/**
+	 * 
+		可以走重复路: 记忆化搜索 -> dp[i][j][k]表示：必须以m[i][j]这个字符结尾的情况下，能不能找到w[0...k]这个前缀串
+	 */
+	public static boolean findWord1(char[][] m, String word) {
+		if (word == null || word.equals("")) {
+			return true;
+		}
+		if (m == null || m.length == 0 || m[0] == null || m[0].length == 0) {
+			return false;
+		}
+		char[] w = word.toCharArray();
+		int N = m.length;
+		int M = m[0].length;
+		int len = w.length;
+		// dp[i][j][k]表示：必须以m[i][j]这个字符结尾的情况下，能不能找到w[0...k]这个前缀串
+		boolean[][][] dp = new boolean[N][M][len];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				dp[i][j][0] = m[i][j] == w[0];
+			}
+		}
+		for (int k = 1; k < len; k++) {
+			for (int i = 0; i < N; i++) {
+				for (int j = 0; j < M; j++) {
+					dp[i][j][k] = (m[i][j] == w[k] && checkPrevious(dp, i, j, k));
+				}
+			}
+		}
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < M; j++) {
+				if (dp[i][j][len - 1]) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	public static boolean checkPrevious(boolean[][][] dp, int i, int j, int k) {
+		boolean up = i > 0 ? (dp[i - 1][j][k - 1]) : false;
+		boolean down = i < dp.length - 1 ? (dp[i + 1][j][k - 1]) : false;
+		boolean left = j > 0 ? (dp[i][j - 1][k - 1]) : false;
+		boolean right = j < dp[0].length - 1 ? (dp[i][j + 1][k - 1]) : false;
+		return up || down || left || right;
 	}
 
 }

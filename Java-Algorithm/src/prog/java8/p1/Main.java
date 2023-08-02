eb87a94b9aa72f9788efa5fcf80b16b572d418a8
Song Lei <songlei198974@gmail.com>
2023-08-03 04:43:02 +0800

    compilation for java8

--- Java-Algorithm/src/prog/java8/p1/Main.java
+++ Java-Algorithm/src/prog/java8/p1/Main.java
@@ -3,12 +3,12 @@ package prog.java8.p1;
 public class Main {
 
 	public static void main(String[] args) {
-	 	var sum1 = Functions.def((Integer x) -> x + x);
-        var sum2 = Functions.def((Integer x) -> (Integer y) -> x + y);
-        var sum3 = Functions.def((Integer x) -> (Integer y) -> (Integer z) -> x + y + z);
-
-        System.out.println("sum 1 : " + sum1.apply(1));
-        System.out.println("sum 2 : " + sum2.apply(1).apply(2));
-        System.out.println("sum 3 : " + sum3.apply(1).apply(2).apply(3));
+//	 	var sum1 = Functions.def((Integer x) -> x + x);
+//        var sum2 = Functions.def((Integer x) -> (Integer y) -> x + y);
+//        var sum3 = Functions.def((Integer x) -> (Integer y) -> (Integer z) -> x + y + z);
+//
+//        System.out.println("sum 1 : " + sum1.apply(1));
+//        System.out.println("sum 2 : " + sum2.apply(1).apply(2));
+//        System.out.println("sum 3 : " + sum3.apply(1).apply(2).apply(3));
 	}
 }

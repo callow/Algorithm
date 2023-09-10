365be6dd6f78a9dbbe66b59b5032303c4836b21d
Song Lei <songlei198974@gmail.com>
2023-09-11 06:29:14 +0800

    a

---
+++ Java-Algorithm/src/com/algo/tasks/day15/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,24 @@
+package com.algo.tasks.day15;
+
+/**
+ * 股票问题1：
+ *  https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ * 
+ */
+public class BestTimeToBuyAndSellStock {
+
+	public static int maxProfit(int[] prices) {
+		if (prices == null || prices.length == 0) {
+			return 0;
+		}
+		// 必须在0时刻卖掉，[0] - [0]
+		int ans = 0;
+		// arr[0...0]
+		int min = prices[0];
+		for (int i = 1; i < prices.length; i++) {
+			min = Math.min(min, prices[i]);
+			ans = Math.max(ans, prices[i] - min);
+		}
+		return ans;
+	}
+}

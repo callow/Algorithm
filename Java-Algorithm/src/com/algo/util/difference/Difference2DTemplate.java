cda32052ba997cba7762350c02a7f45c89db5fa0
leisong <leisong@leidembp>
2024-01-22 07:54:52 +0800

    a

---
+++ Java-Algorithm/src/com/algo/util/difference/Difference2DTemplate.java
@@ -0,0 +1,87 @@
+package com.algo.util.difference;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.StreamTokenizer;
+
+/**
+ * Matrix Difference Template:
+ * 
+ * https://www.luogu.com.cn/problem/P3397
+ */
+public class Difference2DTemplate {
+
+	public static int MAXN = 1002;
+
+	public static int[][] diff = new int[MAXN][MAXN];
+
+	public static int n, q;
+
+	/**
+	 * 四角v操作
+	 */
+	public static void add(int a, int b, int c, int d, int k) {
+		diff[a][b] += k;
+		diff[c + 1][b] -= k;
+		diff[a][d + 1] -= k;
+		diff[c + 1][d + 1] += k;
+	}
+
+	/**
+	 * 最后build一下 得到最终的答案。
+	 */
+	public static void build() {
+		for (int i = 1; i <= n; i++) {
+			for (int j = 1; j <= n; j++) {
+				diff[i][j] += diff[i - 1][j] + diff[i][j - 1] - diff[i - 1][j - 1];
+			}
+		}
+	}
+
+	public static void clear() {
+		for (int i = 1; i <= n + 1; i++) {
+			for (int j = 1; j <= n + 1; j++) {
+				diff[i][j] = 0;
+			}
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StreamTokenizer in = new StreamTokenizer(br);
+		PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+		while (in.nextToken() != StreamTokenizer.TT_EOF) {
+			n = (int) in.nval;
+			in.nextToken();
+			q = (int) in.nval;
+			for (int i = 1, a, b, c, d; i <= q; i++) {
+				in.nextToken();
+				a = (int) in.nval;
+				in.nextToken();
+				b = (int) in.nval;
+				in.nextToken();
+				c = (int) in.nval;
+				in.nextToken();
+				d = (int) in.nval;
+				add(a, b, c, d, 1); // 同样做四角v操作，只是矩阵就1个数字而已
+			}
+			build(); // 统一构建答案数组
+			
+			// 打印答案
+			for (int i = 1; i <= n; i++) {
+				out.print(diff[i][1]);
+				for (int j = 2; j <= n; j++) {
+					out.print(" " + diff[i][j]);
+				}
+				out.println();
+			}
+			clear();
+		}
+		out.flush();
+		out.close();
+		br.close();
+	}
+}

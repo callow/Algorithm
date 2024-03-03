32ab33c405ac12f30c4cf657d829ac37ffbf5032
Song Lei <songlei198974@gmail.com>
2024-03-04 01:59:51 +0800

    a

--- Java-Algorithm/src/com/algo/util/common/CommonNumberUtil.java
+++ Java-Algorithm/src/com/algo/util/common/CommonNumberUtil.java
@@ -47,6 +47,20 @@ public class CommonNumberUtil {
 		}
 		return count;
 	}
+	
+	/**
+	 * ·´×ªÊı×Ö
+	 */
+	
+	public static int reverse(int number) {
+		int reverse = 0;
+		for(;number != 0;) {  
+			int remainder = number % 10;  
+			reverse = reverse * 10 + remainder;  
+			number=number/10;  
+		}
+		return reverse;
+	}
 
 	/**
 	 * 

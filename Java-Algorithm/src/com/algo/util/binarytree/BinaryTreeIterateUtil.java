81a848c1476ca87b060758f05bb0ea602f4271fb
leisong <leisong@leidembp>
2024-03-18 07:39:53 +0800

    s

--- Java-Algorithm/src/com/algo/util/binarytree/BinaryTreeIterateUtil.java
+++ Java-Algorithm/src/com/algo/util/binarytree/BinaryTreeIterateUtil.java
@@ -370,4 +370,29 @@ public class BinaryTreeIterateUtil {
 		return new BTNode(Integer.valueOf(val));
 	}
 	
+		/**
+		 * 二叉树是否是对称的？
+		    symmetric:
+		        1. left head val = right head val
+		        2. left.left.val = right.right.val && left.right.val = right.left.val
+		 */
+		public static boolean isSymmetric(BTNode root) {
+		    return isMirror(root, root);
+		}
+		
+		// 一棵树是原始树  head1
+		// 另一棵是翻面树  head2
+		public static boolean isMirror(BTNode head1, BTNode head2) {
+			if (head1 == null && head2 == null) {
+				return true;
+			}
+			if (head1 != null && head2 != null) {
+				return head1.value == head2.value 
+						&& isMirror(head1.left, head2.right) 
+						&& isMirror(head1.right, head2.left);
+			}
+			// 一个为空，一个不为空  false
+			return false;
+		}
+	
 }

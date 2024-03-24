6da8af0327e98280382497204ba37a8729043bea
leisong <leisong@leidembp>
2024-03-25 02:21:23 +0800

    s

--- Java-Algorithm/src/com/algo/util/floodfill/FloodFillUtil.java
+++ Java-Algorithm/src/com/algo/util/floodfill/FloodFillUtil.java
@@ -79,5 +79,78 @@ public class FloodFillUtil {
 	}
 	
 	
+	/**
+	 * 0 = water, 1 = island, 只允许flip一个0 让连成的岛最大
+	 * 
+	 * https://leetcode.com/problems/making-a-large-island/description/
+	 * 
+	 * 1. 使用Floodfill给每一片岛标号：1，2，3，4. 并统计个数
+	 * 2. 遍历剩下的0，统计连成最大的岛
+	 */
+	public static int largestIslandAfterFlip(int[][] grid) {
+		int n = grid.length;
+		int m = grid[0].length;
+		int id = 2;
+		
+		// 给岛屿标号
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 1) {
+					Util.floodFill3(grid, id, j, id++);
+				}
+			}
+		}
+		
+		// 收集每个编号岛大小
+		int[] sizes = new int[id];
+		int ans = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] > 1) {
+					ans = Math.max(ans, ++sizes[grid[i][j]]);
+				}
+			}
+		}
+		
+		// 讨论所有的0，变成1，能带来的最大岛的大小
+		boolean[] visited = new boolean[id];
+		int up, down, left, right, merge; // 上面是哪个岛的编号，下面是哪个岛的编号
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (grid[i][j] == 0) {
+					up = i > 0 ? grid[i - 1][j] : 0;
+					down = i + 1 < n ? grid[i + 1][j] : 0;
+					left = j > 0 ? grid[i][j - 1] : 0;
+					right = j + 1 < m ? grid[i][j + 1] : 0;
+					visited[up] = true;
+					merge = 1 + sizes[up];
+					if (!visited[down]) {
+						merge += sizes[down];
+						visited[down] = true;
+					}
+					if (!visited[left]) {
+						merge += sizes[left];
+						visited[left] = true;
+					}
+					if (!visited[right]) {
+						merge += sizes[right];
+						visited[right] = true;
+					}
+					ans = Math.max(ans, merge); // 变后收集最大的岛屿
+					visited[up] = false;
+					visited[down] = false;
+					visited[left] = false;
+					visited[right] = false;
+				}
+			}
+		}
+		return ans;
+	}
+	
+	
+	
+	
+	
+	
 	
 }
--- Java-Algorithm/src/com/algo/util/floodfill/Util.java
+++ Java-Algorithm/src/com/algo/util/floodfill/Util.java
@@ -38,4 +38,17 @@ public class Util {
 		floodFill2(board,  i, j - 1); // 左
 		floodFill2(board, i, j + 1); // 右
 	}
+	
+	public static void floodFill3(int[][] board, int i, int j, int id) {
+		int n = board.length;
+		int m = board[0].length;
+		if (i < 0 || i == n || j < 0 || j == m || board[i][j] != 1) {
+			return;
+		}
+		board[i][j] = id; // 感染成随便什么都可以的ascci code
+		floodFill3(board, i - 1, j,id); // 上
+		floodFill3(board, i + 1, j,id); // 下
+		floodFill3(board,  i, j - 1,id); // 左
+		floodFill3(board, i, j + 1,id); // 右
+	}
 }

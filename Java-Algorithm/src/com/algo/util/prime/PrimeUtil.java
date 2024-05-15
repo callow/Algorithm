8c84b47977d92d91a55bec73ee47df7d8c8f1da4
Song Lei <songlei198974@gmail.com>
2024-05-16 06:39:54 +0800

    a

--- Java-Algorithm/src/com/algo/util/prime/PrimeUtil.java
+++ Java-Algorithm/src/com/algo/util/prime/PrimeUtil.java
@@ -1,6 +1,8 @@
 package com.algo.util.prime;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 质数：除了自己和1无法被其他数分解
@@ -41,6 +43,26 @@ public class PrimeUtil {
 		return c.isProbablePrime(s);
 	}
 	
+	
+	/**
+	 * 质因子分解： 10 -> [2 , 5]  O(√n)
+	 *  任意给一个数，打印所有质数因子
+	 */
+	public static List<Integer> primeFactors(int n) {
+		List<Integer> r = new ArrayList<>();
+		for (int i = 2; i * i <= n; i++) {
+			if (n % i == 0) {
+				r.add(i);
+				while (n % i == 0) {
+					n /= i;
+				}
+			}
+		}
+		if (n > 1) {
+			r.add(n);
+		}
+		return r;
+	}
 	
 	
 	

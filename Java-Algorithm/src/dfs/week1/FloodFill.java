a3e810beb946d8695dfa31543dde59552e690b5e
Song Lei <songlei198974@gmail.com>
2025-06-19 22:05:12 +0800

    a

--- Java-Algorithm/src/dfs/week1/FloodFill.java
+++ Java-Algorithm/src/dfs/week1/FloodFill.java
@@ -14,8 +14,8 @@ public class FloodFill {
 	            {'0', '0', '1', '1', '0'},
 	            {'0', '0', '0', '0', '0'}
 	        };
-//		floodFill(image, 2, 1);
-		floodFill2(image, 2, 1);
+//		dfs(image, 2, 1);
+		dfs2(image, 2, 1);
 		
 		// 打印最终结果：
 		for (char[] row : image) {
@@ -31,7 +31,7 @@ public class FloodFill {
 	 * 
 	 * 感染==‘0’部分
 	 */
-	public static void floodFill(char[][] board, int i, int j) {
+	public static void dfs(char[][] board, int i, int j) {
 		int n = board.length;
 		int m = board[0].length;
 		if (i < 0 || i == n || j < 0 || j == m || board[i][j] != '0') {
@@ -39,16 +39,16 @@ public class FloodFill {
 		}
 		System.out.println("Filling: " + i + ", " + j);
 		board[i][j] = 'Z'; // 初始感染点： 感染成随便什么都可以的ascci code
-		floodFill(board, i - 1, j); // 上
-		floodFill(board, i + 1, j); // 下
-		floodFill(board,  i, j - 1); // 左
-		floodFill(board, i, j + 1); // 右
+		dfs(board, i - 1, j); // 上
+		dfs(board, i + 1, j); // 下
+		dfs(board,  i, j - 1); // 左
+		dfs(board, i, j + 1); // 右
 	}
 	
 	/**
 	 * 利用方向矩阵 + 1个dfs
 	 */
-	public static void floodFill2(char[][] board, int i, int j) {
+	public static void dfs2(char[][] board, int i, int j) {
 		int n = board.length;
 		int m = board[0].length;
 		
@@ -62,7 +62,7 @@ public class FloodFill {
 	        int jj = j + directionMatrix[d + 1];
 	        
 	        if(ii >= 0 && ii < n && jj >= 0 && jj < m && board[ii][jj] == '0') {
-	        	floodFill2(board, ii, jj);
+	        	dfs2(board, ii, jj);
 	        }
 		}
 	}
---
+++ Java-Algorithm/src/dfs/week1/MaxAreaOfIsland.java
@@ -0,0 +1,45 @@
+package dfs.week1;
+
+public class MaxAreaOfIsland {
+
+	
+	public static void main(String[] args) {
+		char[][] grid = {
+	            {'1', '1', '0', '0', '0'},
+	            {'1', '1', '0', '0', '0'},
+	            {'0', '0', '1', '1', '0'},
+	            {'0', '0', '1', '1', '0'},
+	            {'0', '0', '0', '0', '0'}};
+	            
+		System.out.println(maxAreaOfIsland(grid));
+	}
+	
+	public static int maxAreaOfIsland(char[][] grid) {
+	   int n = grid.length;
+	   int m = grid[0].length;
+	   int area = 0;
+	   for(int i = 0; i < n; i++) {
+		   for(int j = 0; j < m; j++) {
+			   if (grid[i][j] == '1') {
+				   int beforeDFSLargestArea = area;
+				   int afterDFSLargestArea = dfs(grid,n,m,i,j);
+				   area = Math.max(beforeDFSLargestArea, afterDFSLargestArea);
+			   }
+		   }
+	   }
+	   return area;
+	}
+	
+	public static int dfs(char[][] grid, int n , int m , int i, int j) {
+		if (i < 0 || i == n || j < 0 || j == m || grid[i][j] != '1') {
+			return 0;
+		}
+		int area = 1; // 当前格子算1
+		grid[i][j] = '0';
+		area += dfs(grid, n,m, i - 1, j); 
+		area += dfs(grid, n,m, i + 1, j); 
+		area += dfs(grid, n,m, i, j - 1); 
+		area += dfs(grid, n,m, i, j + 1); 
+		return area;
+	}
+}

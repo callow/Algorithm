c3d5bf7ea773861324b03e5613fbce3474d2a822
Song Lei <songlei198974@gmail.com>
2025-06-21 07:29:54 +0800

    a

--- Java-Algorithm/src/dfs/week1/GenerateParentheses.java
+++ Java-Algorithm/src/dfs/week1/GenerateParentheses.java
@@ -1,10 +1,12 @@
 package dfs.week1;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
- * 类型： 带路径递归 需要剪枝，左右括号平衡
+ * 类型： 带路径递归 需要剪枝，左右括号平衡, 回溯
  */
 public class GenerateParentheses {
 
@@ -16,12 +18,30 @@ public class GenerateParentheses {
 	}
 	
 	 public static List<String> generateParenthesis(int n) {
-		 List<String> result = new ArrayList<>();
-	     dfs(n, result);
+		 Deque<String> path = new ArrayDeque<>();
+	     dfs(n, path, 0 , 0);
 	     return result;
 	 }
 	 
-	 public static void dfs(int n, List<String> result) {
+	 /**
+	  *  继续： left > right, 否则就继续+left (
+	  *  终止条件： left = right = 3
+	  */
+	 static List<String> result = new ArrayList<>();
+	 public static void dfs(int n, Deque<String> cur, int leftNo, int rightNo) {
+		 if (leftNo == n && rightNo == n) {
+			 result.add(String.join("", cur));
+		 }
+		 if (leftNo < n) {
+			 cur.add("("); // 尝试+ (
+			 dfs(n, cur, leftNo + 1, rightNo);
+			 cur.removeLast();
+		 }
 		 
+		 if (leftNo > rightNo) {
+			 cur.add(")"); // 尝试+ )
+			 dfs(n, cur, leftNo, rightNo + 1);
+			 cur.removeLast();
+		 }
 	 }
 }
